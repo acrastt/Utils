ae7f06bfd4b68ca6a381da4baa0a35f684cc4135
Bohan Du <bohandu@hotmail.com>
2023-02-26 20:08:56 -0800

    comm

--- src/main/java/utils/JMHUtils.java
+++ src/main/java/utils/JMHUtils.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The utility class for JMH(Java Micro-benchmarking Harness).
+ * The utility class for JMH.
  *
  * @author Bohan Du
  * @version 1.0
