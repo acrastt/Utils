85075dba591da3a272916b056be85a5d3e65f371
Bohan Du <bohandu@hotmail.com>
2023-03-18 17:58:54 -0700

    comm

--- src/main/java/org/example/acrastt/utils/ConcurrentList.java
+++ src/main/java/org/example/acrastt/utils/ConcurrentList.java
@@ -2,8 +2,6 @@ package org.example.acrastt.utils;
 
 
 import org.apache.commons.lang3.concurrent.ConcurrentRuntimeException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.Serial;
 import java.util.*;
