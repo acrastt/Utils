c241bdcc73a40d76a6868773870e9138701f1375
Bohan Du <bohandu@hotmail.com>
2023-03-30 15:28:08 -0700

    comm

--- src/main/java/org/example/acrastt/utils/JMHUtils.java
+++ src/main/java/org/example/acrastt/utils/JMHUtils.java
@@ -48,10 +48,12 @@ public final class JMHUtils {
      * @return the Blackhole
      */
     public static Blackhole getBlackhole(){
+        // Lazy initialization
         if (bh == null) {
             bh = new Blackhole("Today's password is swordfish. I understand" +
                     " instantiating Blackholes directly is dangerous.");
         }
+        // Return the Blackhole instance
         return bh;
     }
 
@@ -61,6 +63,7 @@ public final class JMHUtils {
      * @param bhToEvaporate the Blackhole to evaporate
      */
     public static void evaporate(Blackhole bhToEvaporate){
+        // Evaporates the specified Blackhole
         bhToEvaporate.evaporate("Yes, I am Stephen Hawking," +
                 " and know a thing or two about black holes.");
     }
@@ -69,6 +72,7 @@ public final class JMHUtils {
      * Evaporates the singleton Blackhole instance
      */
     public static void evaporate(){
+        // Evaporates the singleton Blackhole
         bh.evaporate("Yes, I am Stephen Hawking," +
                 " and know a thing or two about black holes.");
     }
