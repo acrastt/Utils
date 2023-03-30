715bd5f81df02cce46926f09cc35a0222d131d4b
Bohan Du <bohandu@hotmail.com>
2023-03-30 15:48:30 -0700

    comm

--- src/main/java/org/example/acrastt/utils/NumberUtils.java
+++ src/main/java/org/example/acrastt/utils/NumberUtils.java
@@ -1,7 +1,5 @@
 package org.example.acrastt.utils;
 
-import org.apache.commons.math3.util.FastMath;
-
 /**
  * The utility class for numbers(int, double, long, etc.).
  *
@@ -26,7 +24,7 @@ public final class NumberUtils {
      */
     public static String convertToOrdinal(int number) {
         // Gets the last two digits of the absolute value of number
-        int lastTwoDigits = FastMath.abs(number) % 100;
+        int lastTwoDigits = Math.abs(number) % 100;
         // If the last two digits are 11 or 13, we know the suffix is "th".
         // So return number and "th" is the right thing to do
         if (11 <= lastTwoDigits && lastTwoDigits <= 13) {
@@ -45,7 +43,7 @@ public final class NumberUtils {
      */
     private static String returnOrdinal(int number) {
         // Gets the last digit
-        int lastDigit = FastMath.abs(number) % 10;
+        int lastDigit = Math.abs(number) % 10;
         // Return the suffix based on different situations
         return switch (lastDigit) {
             case 1 -> "st";
